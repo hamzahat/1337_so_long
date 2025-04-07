9cef127089a8a6cb5642bfa55c7468915f05be34
Hamza Benmohamd <hbenmoha@e2r9p15.1337.ma>
2025-04-07 01:12:13 +0100

    add new map files

--- main.c
+++ main.c
@@ -6,18 +6,18 @@
 /*   By: hbenmoha <hbenmoha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/28 18:29:17 by hbenmoha          #+#    #+#             */
-/*   Updated: 2025/04/06 23:17:41 by hbenmoha         ###   ########.fr       */
+/*   Updated: 2025/04/07 00:56:03 by hbenmoha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
 // void f(){system("leaks so_long");} // ? to test memory leaks
-void f(){system("lsof -c so_long");} //? to test if any file descriptor is opend 
+// void f(){system("lsof -c so_long");} //? to test if any file descriptor is opend 
 
 int main(int ac, char *av[])
 {
-	atexit(f);
+	// atexit(f);
 	t_game	game;                   // Main game structure
 
 //? Parse command line arguments and map file
